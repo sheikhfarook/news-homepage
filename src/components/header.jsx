5d7712c65e01d76d4c2f422ff17120d75b9c240a
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-01-18 14:38:42 +0530

    header complete

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,22 +1,22 @@
 import letter from "../assets/W..svg";
 const Header = () => {
   return (
-    <div>
+    <div className="flex justify-between m-20 mx-44">
       <div>
         <img
-          className="w-[ 4.00575rem] h-[ 2.49375rem] shrink-0"
+          className="w-[4.00575rem] h-[2.49375rem] shrink-0"
           src={letter}
           alt=""
         />
       </div>
       <div
         className="text-[#5E607A] text-[0.9375rem] 
-      font-[400] leading-[1.625rem] flex gap-8">
-        <div>Home</div>
-        <div>New</div>
-        <div>Popular</div>
-        <div>Trending</div>
-        <div>Categories</div>
+      font-[400] leading-[1.625rem] flex gap-8 cursor-pointer ">
+        <div className="hover:text-[#F15D51]">Home</div>
+        <div className="hover:text-[#F15D51]">New</div>
+        <div className="hover:text-[#F15D51]">Popular</div>
+        <div className="hover:text-[#F15D51]">Trending</div>
+        <div className="hover:text-[#F15D51]">Categories</div>
       </div>
     </div>
   );
