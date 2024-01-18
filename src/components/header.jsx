919a4d2f11b24c2f71eac77a4a80bca505cfc296
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-01-18 12:54:51 +0530

    complete the header code and css code also added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,25 @@
-import React from "react";
-
+import letter from "../assets/W..svg";
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <img
+          className="w-[ 4.00575rem] h-[ 2.49375rem] shrink-0"
+          src={letter}
+          alt=""
+        />
+      </div>
+      <div
+        className="text-[#5E607A] text-[0.9375rem] 
+      font-[400] leading-[1.625rem] flex gap-8">
+        <div>Home</div>
+        <div>New</div>
+        <div>Popular</div>
+        <div>Trending</div>
+        <div>Categories</div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
